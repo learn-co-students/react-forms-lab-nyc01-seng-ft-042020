77b113559b2606315ea7b1bffe3a7b1a05fca0b6
Srishti Nath <srishti.nath@gmail.com>
2020-06-28 02:28:36 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
 
-  login = ({ username, password }) => {
-    console.log(`Logging in ${username} with password ${password}`);
+  login = (event) => {
+    event.preventDefault();
+    console.log("Logged!")
   };
 
   render() {
@@ -15,7 +16,7 @@ class App extends Component {
         <h1>
           <pre>LoginForm</pre>
         </h1>
-        <LoginForm handleLogin={this.login} />
+        <LoginForm handleLogin={this.login}/>
 
         <h1>
           <pre>TwitterMessage</pre>
